8cdb049788c0673e9928b27fc2c733255c5bebed
ReineDeLaNight <germain40k@hotmail.fr>
2020-04-15 11:51:52 +0200

    Ajout dossier Images + résolution écran pour plein écran

--- test.py
+++ test.py
@@ -2,17 +2,26 @@ import pygame
 class window():
 
     def __init__(self):
-        title = pygame.display.set_caption("comet fall game")
-        self._screen = pygame.display.set_mode((1080, 720))
-        self._background = pygame.image.load('bak.png')
+        title = "comet fall game"
+        pygame.display.set_caption(title)
+
+        self._w, self._h = self.getSize()
+
+        self._screen = pygame.display.set_mode((self._w, self._h), pygame.FULLSCREEN)
+        self._background = pygame.image.load('./Images/fond.jpg')
 
         run = True
         while run:
-            self._screen.blit(self._background, (500,550))
+            self._screen.blit(self._background, (0, 0))
             pygame.display.flip()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
                     pygame.quit()
+    
+    def getSize(self):
+        return pygame.display.Info().current_w, pygame.display.Info().current_h
+
+        
 pygame.init()
 window()
