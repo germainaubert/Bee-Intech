e869567da79d6add539af7c5792d6f78259cb5f4
Leo.g <Leo.g@Univers1000.FR>
2020-04-16 10:29:21 +0200

    init des class de bases du jeu

---
+++ Bees.py
@@ -0,0 +1,8 @@
+
+class bees():
+
+	def __init__(self):
+
+		self.cost = 0
+		self.category = None
+		self.upgrade = []
---
+++ Hive.py
@@ -0,0 +1,12 @@
+
+class hive():
+
+	def __init__(self):
+
+	self.level = 0
+	self.exp = 0
+	self.honey = 10
+	self.honey_prod = 0
+	self.pollen = 0
+	self.bees = []
+	self.upgrades = []
