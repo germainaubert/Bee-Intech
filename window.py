af2f83025c78204a53d3e0f81ad1638bc162780d
ReineDeLaNight <germain40k@hotmail.fr>
2020-04-16 11:08:56 +0200

    Modifs sur Window.py séparation de l'affichage du menu, du mainloop, et de l'init

--- window.py
+++ window.py
@@ -1,36 +1,43 @@
 import pygame
-from create_button import button
+from pygame.locals import *
+from Create_button import button
 
 class window():
 
     def __init__(self):
         pygame.init()
-
+        
         self._w, self._h = self.getSize()
 
         self._window = pygame.display.set_mode((self._w, self._h), pygame.FULLSCREEN)
 
-        self.title = "comet fall game"
-        pygame.display.set_caption(self.title)
+        self._title = "comet fall game"
+        pygame.display.set_caption(self._title)
+        self.display_menu()
 
+    def display_menu(self):
         self._background = pygame.image.load('./Images/fond.jpg')
-
+        self._window.blit(self._background, (0, 0))
+        self._quit_button = button((212,180,0), 150, 225, 250, 100, 'Quitter')
+        self._quit_button.draw_button(self._window)
+        
+    def main_loop(self):
         run = True
-
-        #Créer un bouton
-        quit_button = button((0,255,0), 150, 225, 250, 100, 'Quitter')
-
         while run:
-            self._window.blit(self._background, (0, 0))
-            quit_button.draw_button(self._window)
             pygame.display.flip()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
                     pygame.quit()
-    
+                if event.type == MOUSEBUTTONDOWN:
+                    if self._quit_button.is_over(event.pos):
+                        run = False
+                        pygame.quit()
+            
+        
+                
     def getSize(self):
         return pygame.display.Info().current_w, pygame.display.Info().current_h
 
-pygame.init()
-window()
+window = window()
+window.main_loop()
